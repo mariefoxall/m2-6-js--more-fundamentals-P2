14074e44ab2db08df0b5ea840bb66ba0f3446a50
Marie Foxall <marie@wastedeffort.ca>
2020-06-23 13:04:37 -0400

    Exercise-3

--- workshop/exercise-3.js
+++ workshop/exercise-3.js
@@ -2,14 +2,26 @@
 // ----------
 
 let mostPopularFood = [
-  'bean burritos',
-  'poke bowl',
-  'chicken sliders',
-  'pork baby back ribs',
-  'chicken burritos',
-  'chicken sandwich',
-  'cauliflower rice bowl',
-  'chicken and waffle sliders',
-  'parmesan chicken',
-  'buffalo-flavored cauliflower',
+  "bean burritos",
+  "poke bowl",
+  "chicken sliders",
+  "pork baby back ribs",
+  "chicken burritos",
+  "chicken sandwich",
+  "cauliflower rice bowl",
+  "chicken and waffle sliders",
+  "parmesan chicken",
+  "buffalo-flavored cauliflower",
 ];
+
+const printAlphabeticalFood = (array) => {
+  let rankedPopularFood = array.map(function (currentValue, index) {
+    return `${currentValue} (${index + 1})`;
+  });
+  let newArray = rankedPopularFood.sort();
+  newArray.forEach(function (element) {
+    console.log(element);
+  });
+};
+
+printAlphabeticalFood(mostPopularFood);
