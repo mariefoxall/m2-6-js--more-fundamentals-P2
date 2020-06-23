1f8c472b7afdd5816be6da2f7e45d098395f203c
Marie Foxall <marie@wastedeffort.ca>
2020-06-23 12:49:20 -0400

    Exercise-2

--- workshop/exercise-2.js
+++ workshop/exercise-2.js
@@ -1,5 +1,15 @@
 // Exercise 2
 // ----------
 
-const lateComers = ['Felicia', 'Charles', 'Terry'];
-const earlyBirds = ['Ellen', 'Jerry', 'Jasmine'];
+const lateComers = ["Felicia", "Charles", "Terry"];
+const earlyBirds = ["Ellen", "Jerry", "Jasmine"];
+
+const printGuestOrder = (early, late) => {
+  const allGuests = [...early, ...late];
+  allGuests.forEach(function (element, index) {
+    console.log(`${index + 1}. ${element}`);
+  });
+  return allGuests;
+};
+
+printGuestOrder(earlyBirds, lateComers);
