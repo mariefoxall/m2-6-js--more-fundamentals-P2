0ce3f14a6144eef09c0cc8429139568072f7e556
Marie Foxall <marie@wastedeffort.ca>
2020-06-23 13:12:07 -0400

    Exercise 4

--- workshop/exercise-4.js
+++ workshop/exercise-4.js
@@ -2,14 +2,17 @@
 // ----------
 
 let foodPairings = {
-  burgers: 'beer',
-  fish: 'white wine',
-  beef: 'red wine',
-  strawberries: 'cream',
+  burgers: "beer",
+  fish: "white wine",
+  beef: "red wine",
+  strawberries: "cream",
 };
 
 const converter = (obj) => {
-  // do something
+  const food = Object.keys(obj);
+  const drink = Object.values(obj);
+  const Pairings = [food, drink];
+  return Pairings;
 };
 
 console.log(converter(foodPairings));
