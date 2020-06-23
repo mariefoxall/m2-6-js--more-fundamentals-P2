193e1e6514f8a3fa9719387269dd09d3841dd654
Marie Foxall <marie@wastedeffort.ca>
2020-06-23 14:30:57 -0400

    Exercise-6

--- workshop/exercise-6.js
+++ workshop/exercise-6.js
@@ -3,8 +3,8 @@
 
 const staffMembers = [
   {
-    name: 'Jane Smith',
-    title: 'Web Developer II',
+    name: "Jane Smith",
+    title: "Web Developer II",
     skillLevels: {
       javascript: 7,
       python: 4,
@@ -12,8 +12,8 @@ const staffMembers = [
     },
   },
   {
-    name: 'Frank Castle',
-    title: 'Web Developer I',
+    name: "Frank Castle",
+    title: "Web Developer I",
     skillLevels: {
       javascript: 4,
       python: 4,
@@ -21,8 +21,8 @@ const staffMembers = [
     },
   },
   {
-    name: 'Steve Rogers',
-    title: 'Database Admin',
+    name: "Steve Rogers",
+    title: "Database Admin",
     skillLevels: {
       javascript: 7,
       python: 4,
@@ -30,8 +30,8 @@ const staffMembers = [
     },
   },
   {
-    name: 'Chuck Taylor',
-    title: 'Web Developer III',
+    name: "Chuck Taylor",
+    title: "Web Developer III",
     skillLevels: {
       javascript: 8,
       python: 7,
@@ -39,8 +39,8 @@ const staffMembers = [
     },
   },
   {
-    name: 'Thor Odinson',
-    title: 'Web Intern',
+    name: "Thor Odinson",
+    title: "Web Intern",
     skillLevels: {
       javascript: 4,
       python: 3,
@@ -50,5 +50,18 @@ const staffMembers = [
 ];
 
 const getData = (arr, key) => {
-  // return something
+  let newArray = [];
+  arr.forEach((element) => {
+    // console.log(element);
+    // console.log(key);
+    if (key in element) {
+      newArray.push(element[key]);
+    }
+  });
+  console.log(newArray);
+  return newArray;
 };
+
+getData(staffMembers, "name");
+getData(staffMembers, "title");
+// getData(staffMembers, "skillLevels");
