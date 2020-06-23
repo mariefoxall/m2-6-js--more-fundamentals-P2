2097dc21b95d34d8886a5fed195911495f04599d
Marie Foxall <marie@wastedeffort.ca>
2020-06-23 15:09:38 -0400

    Exercise-7

--- workshop/exercise-7.js
+++ workshop/exercise-7.js
@@ -2,10 +2,26 @@
 // ----------
 
 let foodPairings = {
-  burgers: 'beer',
-  fish: 'white wine',
-  beef: 'red wine',
-  strawberries: 'cream',
+  burgers: "beer",
+  fish: "white wine",
+  beef: "red wine",
+  strawberries: "cream",
+};
+
+const createRecommendations = (obj) => {
+  let keysArray = Object.keys(obj);
+  let sentenceArray = [];
+  keysArray.forEach((key) => {
+    sentenceArray.push(`With ${key}, it is best to have ${obj[key]}.`);
+    // console.log sentenceArray;
+  });
+  return sentenceArray;
+};
+
+const printRecommendations = (arr) => {
+  arr.forEach((element) => {
+    console.log(element);
+  });
 };
 
 // createRecommendations function
