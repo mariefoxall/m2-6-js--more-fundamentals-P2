08603134b2a10508d4f0421eb2511788a4c8fdeb
Marie Foxall <marie@wastedeffort.ca>
2020-06-23 12:42:41 -0400

    Exercise-1

--- workshop/exercise-1.js
+++ workshop/exercise-1.js
@@ -2,13 +2,30 @@
 // ----------
 
 let mostPopularFood = [
-  'poke bowl',
-  'chicken sliders',
-  'pork baby back ribs',
-  'chicken burritos',
-  'chicken sandwich',
-  'cauliflower rice bowl',
-  'chicken and waffle sliders',
-  'parmesan chicken',
+  "poke bowl",
+  "chicken sliders",
+  "pork baby back ribs",
+  "chicken burritos",
+  "chicken sandwich",
+  "cauliflower rice bowl",
+  "chicken and waffle sliders",
+  "parmesan chicken",
   undefined,
 ];
+
+const fixList = (arr, first, last) => {
+  arr.unshift(first);
+  arr.pop();
+  arr.push(last);
+  return arr;
+};
+
+const printList = (arr) => {
+  arr.forEach(function (element, index) {
+    console.log(`${index + 1}. ${element}`);
+  });
+};
+
+printList(
+  fixList(mostPopularFood, "bean burritos", "buffalo-flavoured cauliflower")
+);
